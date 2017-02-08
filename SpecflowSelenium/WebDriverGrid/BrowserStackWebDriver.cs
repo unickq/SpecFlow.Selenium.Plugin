d74ddf21d718035d1f27c8e5c04fef23f51805b8
Nick Chursin <mykyta.chursin@valtech.com>
2017-02-08 13:14:37 +0200

    Added ability to read most BrowserStack, Testingbot, SauceLabs params

--- SpecflowSelenium/WebDriverGrid/BrowserStackWebDriver.cs
+++ SpecflowSelenium/WebDriverGrid/BrowserStackWebDriver.cs
@@ -17,6 +17,24 @@ namespace Unickq.SeleniumHelper.WebDriverGrid
 
         private static readonly string BrowserstackUser = ConfigurationManager.AppSettings["browserstack.user"];
         private static readonly string BrowserstackKey = ConfigurationManager.AppSettings["browserstack.key"];
+        private static readonly string Name = ConfigurationManager.AppSettings["browserstack.name"];
+        private static readonly string Resolution = ConfigurationManager.AppSettings["browserstack.resolution"];
+        private static readonly string Project = ConfigurationManager.AppSettings["browserstack.project"];
+        private static readonly string Build = ConfigurationManager.AppSettings["browserstack.build"];
+        private static readonly string Debug = ConfigurationManager.AppSettings["browserstack.debug"];
+        private static readonly string Video = ConfigurationManager.AppSettings["browserstack.video"];
+        private static readonly string Local = ConfigurationManager.AppSettings["browserstack.local"];
+        private static readonly string Timezone = ConfigurationManager.AppSettings["browserstack.timezone"];
+        private static readonly string LocalIdentifier = ConfigurationManager.AppSettings["browserstack.localIdentifier"];
+        private static readonly string DeviceOrientation = ConfigurationManager.AppSettings["browserstack.deviceOrientation"];
+        private static readonly string SeleniumVersion = ConfigurationManager.AppSettings["browserstack.selenium_version"];
+        private static readonly string NoFlash = ConfigurationManager.AppSettings["browserstack.ie.noFlash"];
+        private static readonly string Compatibility = ConfigurationManager.AppSettings["browserstack.ie.compatibility"];
+        private static readonly string Driver = ConfigurationManager.AppSettings["browserstack.ie.driver"];
+        private static readonly string IeEnablePopups = ConfigurationManager.AppSettings["browserstack.ie.enablePopups"];
+        private static readonly string SafariEnablePopups = ConfigurationManager.AppSettings["browserstack.safari.enablePopups"];
+        private static readonly string SafariAllowAllCookies = ConfigurationManager.AppSettings["browserstack.safari.allowAllCookies"];
+        private static readonly string SafariDriver = ConfigurationManager.AppSettings["browserstack.safari.driver"];
 
         public BrowserStackWebDriver(string browser, Dictionary<string, string> capabilities)
             : base(ApiUrl, browser, Auth(BrowserstackUser, BrowserstackKey, capabilities))
@@ -38,7 +56,30 @@ namespace Unickq.SeleniumHelper.WebDriverGrid
             if (browserstackKey == null) throw new Exception("browserstack.key can't be found");
             capabilities.Add("browserstack.user", browserstackUser);
             capabilities.Add("browserstack.key", browserstackKey);
-            capabilities.Add("name", TestContext.CurrentContext.Test.Name);
+
+            capabilities.Add("name",
+             !string.IsNullOrEmpty(Name)
+                 ? Name
+                 : TestContext.CurrentContext.Test.Name);
+
+            if (!string.IsNullOrEmpty(Resolution)) capabilities.Add("browserstack.resolution", Resolution);
+            if(!string.IsNullOrEmpty(Build)) capabilities.Add("build", Resolution);
+            if(!string.IsNullOrEmpty(Project)) capabilities.Add("project", Project);
+            if(!string.IsNullOrEmpty(Debug)) capabilities.Add("browserstack.debug", Debug);
+            if(!string.IsNullOrEmpty(Video)) capabilities.Add("browserstack.video", Video);
+            if(!string.IsNullOrEmpty(Local)) capabilities.Add("browserstack.local", Local);
+            if(!string.IsNullOrEmpty(DeviceOrientation)) capabilities.Add("deviceOrientation", DeviceOrientation);
+            if(!string.IsNullOrEmpty(SeleniumVersion)) capabilities.Add("browserstack.selenium_version", SeleniumVersion);
+            if(!string.IsNullOrEmpty(LocalIdentifier)) capabilities.Add("browserstack.localIdentifier", LocalIdentifier);
+            if(!string.IsNullOrEmpty(Timezone)) capabilities.Add("browserstack.timezone", NoFlash);
+            if(!string.IsNullOrEmpty(NoFlash)) capabilities.Add("browserstack.ie.noFlash", Timezone);
+            if(!string.IsNullOrEmpty(Compatibility)) capabilities.Add("browserstack.ie.compatibility", Compatibility);
+            if(!string.IsNullOrEmpty(Driver)) capabilities.Add("browserstack.ie.driver", Driver);
+            if(!string.IsNullOrEmpty(IeEnablePopups)) capabilities.Add("browserstack.ie.enablePopups", IeEnablePopups);
+            if(!string.IsNullOrEmpty(SafariEnablePopups)) capabilities.Add("browserstack.safari.enablePopups", SafariEnablePopups);
+            if(!string.IsNullOrEmpty(SafariAllowAllCookies)) capabilities.Add("browserstack.safari.allowAllCookies", SafariAllowAllCookies);
+            if(!string.IsNullOrEmpty(SafariDriver)) capabilities.Add("browserstack.safari.driver", SafariDriver);
+         
             return capabilities;
         }
 
@@ -65,10 +106,6 @@ namespace Unickq.SeleniumHelper.WebDriverGrid
             myHttpWebRequest.PreAuthenticate = true;
             myHttpWebRequest.Credentials = myCredentialCache;
             myWebRequest.GetResponse().Close();
-        }
-
-        public void SetTestName()
-        {
         }
     }
 }
--- SpecflowSelenium/WebDriverGrid/TestingBotWebDriver.cs
+++ SpecflowSelenium/WebDriverGrid/TestingBotWebDriver.cs
@@ -24,6 +24,33 @@ namespace Unickq.SeleniumHelper.WebDriverGrid
         private static readonly string TestingBotKey = ConfigurationManager.AppSettings["testingbot.key"];
         private static readonly string TestingBotSecret = ConfigurationManager.AppSettings["testingbot.secret"];
 
+        private static readonly string Screenshot = ConfigurationManager.AppSettings["testingbot.screenshot"];
+        private static readonly string Video = ConfigurationManager.AppSettings["testingbot.screenrecorder"];
+        private static readonly string SeleniumVersion = ConfigurationManager.AppSettings["testingbot.selenium-version"];
+        private static readonly string ChromedriverVersion = ConfigurationManager.AppSettings["testingbot.chromedriverVersion"];
+        private static readonly string IedriverVersion = ConfigurationManager.AppSettings["testingbot.iedriverVersion"];
+        private static readonly string GeckodriverVersion = ConfigurationManager.AppSettings["testingbot.geckodriverVersion"];
+        private static readonly string Name = ConfigurationManager.AppSettings["testingbot.name"];
+        private static readonly string ScreenResolution = ConfigurationManager.AppSettings["testingbot.screen-resolution"];
+        private static readonly string AvoidProxy = ConfigurationManager.AppSettings["testingbot.avoidProxy"];
+        private static readonly string Build = ConfigurationManager.AppSettings["testingbot.build"];
+        private static readonly string Extra = ConfigurationManager.AppSettings["testingbot.extra"];
+        private static readonly string Idletimeout = ConfigurationManager.AppSettings["testingbot.idletimeout"];
+        private static readonly string Public = ConfigurationManager.AppSettings["testingbot.public"];
+        private static readonly string TimeZone = ConfigurationManager.AppSettings["testingbot.timeZone"];
+        private static readonly string UserExtension  = ConfigurationManager.AppSettings["testingbot.user-extension"];
+        private static readonly string LoadExtension  = ConfigurationManager.AppSettings["testingbot.load-extension"];
+        private static readonly string Groups = ConfigurationManager.AppSettings["testingbot.load-groups"];
+        private static readonly string Prerun = ConfigurationManager.AppSettings["testingbot.prerun"];
+        private static readonly string Upload = ConfigurationManager.AppSettings["testingbot.upload"];
+        private static readonly string UploadFilepath = ConfigurationManager.AppSettings["testingbot.uploadFilepath"];
+        private static readonly string UploadMultiple = ConfigurationManager.AppSettings["testingbot.uploadMultiple"];
+        private static readonly string Autoclick = ConfigurationManager.AppSettings["testingbot.autoclick"];
+        private static readonly string Sikuli = ConfigurationManager.AppSettings["testingbot.sikuli"];
+        private static readonly string Hosts = ConfigurationManager.AppSettings["testingbot.blacklist"];
+        private static readonly string RecordLogs = ConfigurationManager.AppSettings["testingbot.recordLogs"];
+
+
         public TestingBotWebDriver(string browser, Dictionary<string, string> capabilities)
             : base(ApiUrl, browser, Auth(TestingBotKey, TestingBotSecret, capabilities))
         {
@@ -37,7 +64,36 @@ namespace Unickq.SeleniumHelper.WebDriverGrid
             if (secret == null) throw new Exception("testingbot.secret can't be found");
             capabilities.Add("key", key);
             capabilities.Add("secret", secret);
-            capabilities.Add("name", TestContext.CurrentContext.Test.Name);
+
+            capabilities.Add("name",
+                !string.IsNullOrEmpty(Name)
+                ? Name
+                : TestContext.CurrentContext.Test.Name);
+
+            if (!string.IsNullOrEmpty(Screenshot)) capabilities.Add("screenshot", Screenshot);
+            if (!string.IsNullOrEmpty(Video)) capabilities.Add("screenrecorder", Video);
+            if (!string.IsNullOrEmpty(SeleniumVersion)) capabilities.Add("selenium-version", SeleniumVersion);
+            if (!string.IsNullOrEmpty(ChromedriverVersion)) capabilities.Add("chromedriverVersion", ChromedriverVersion);
+            if (!string.IsNullOrEmpty(IedriverVersion)) capabilities.Add("iedriverVersion", IedriverVersion);
+            if (!string.IsNullOrEmpty(GeckodriverVersion)) capabilities.Add("geckodriverVersion", GeckodriverVersion);
+            if (!string.IsNullOrEmpty(ScreenResolution)) capabilities.Add("screen-resolution", ScreenResolution);
+            if (!string.IsNullOrEmpty(AvoidProxy)) capabilities.Add("avoidProxy", AvoidProxy);
+            if (!string.IsNullOrEmpty(Build)) capabilities.Add("build", Build);
+            if (!string.IsNullOrEmpty(Extra)) capabilities.Add("extra", Extra);
+            if (!string.IsNullOrEmpty(Idletimeout)) capabilities.Add("idletimeout", Idletimeout);
+            if (!string.IsNullOrEmpty(Public)) capabilities.Add("public", Public);
+            if (!string.IsNullOrEmpty(TimeZone)) capabilities.Add("timeZone", TimeZone);
+            if (!string.IsNullOrEmpty(UserExtension)) capabilities.Add("user-extension", UserExtension);
+            if (!string.IsNullOrEmpty(LoadExtension)) capabilities.Add("load-extension", LoadExtension);
+            if (!string.IsNullOrEmpty(Groups)) capabilities.Add("load-groups", Groups);
+            if (!string.IsNullOrEmpty(Prerun)) capabilities.Add("prerun", Prerun);
+            if (!string.IsNullOrEmpty(Upload)) capabilities.Add("upload", Upload);
+            if (!string.IsNullOrEmpty(UploadFilepath)) capabilities.Add("uploadFilepath", UploadFilepath);
+            if (!string.IsNullOrEmpty(UploadMultiple)) capabilities.Add("uploadMultiple", UploadMultiple);
+            if (!string.IsNullOrEmpty(Autoclick)) capabilities.Add("autoclick", Autoclick);
+            if (!string.IsNullOrEmpty(Sikuli)) capabilities.Add("sikuli", Sikuli);
+            if (!string.IsNullOrEmpty(Hosts)) capabilities.Add("hosts", Hosts);
+            if (!string.IsNullOrEmpty(RecordLogs)) capabilities.Add("recordLogs", RecordLogs);
             return capabilities;
         }
 
